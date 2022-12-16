8b8b15c88ad5c947d6669da7f953446820a50a41
LuisJann <giannoneluigi10@gmail.com>
2022-12-16 10:03:56 +0100

    Add style Footer and fix ("home") link

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,6 +1,26 @@
-<footer>
-    <div class="container">
+<footer class="bg-dark mt-1">
+    <div class="container d-flex text-light">
+        <div>
+            <ul class="d-flex flex-column">
+                <h2>Titolo</h2>
+                <li class="mx-3"><a href=""></a>ciao</li>
+                <li class="mx-3"><a href=""></a>ciao</li>
+                <li class="mx-3"><a href=""></a>ciao</li>
+                <li class="mx-3"><a href=""></a>ciao</li>
+                <li class="mx-3"><a href=""></a>ciao</li>
+                <li class="mx-3"><a href=""></a>ciao</li>
+                <li class="mx-3"><a href=""></a>ciao</li>
+                <li class="mx-3"><a href=""></a>ciao</li>
+            </ul>
+            <ul class="d-flex flex-column">
+                <h2>Titolo</h2>
+                <li class="mx-3"><a href=""></a>ciao</li>
+                <li class="mx-3"><a href=""></a>ciao</li>
+                <li class="mx-3"><a href=""></a>ciao</li>
+            </ul>
+        </div>
         <ul class="d-flex flex-column">
+            <h2>Titolo</h2>
             <li class="mx-3"><a href=""></a>ciao</li>
             <li class="mx-3"><a href=""></a>ciao</li>
             <li class="mx-3"><a href=""></a>ciao</li>
